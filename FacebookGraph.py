af51a1588a09f3f1904f6af0f18a21c1c64dbfc4
PhoebusProgrammer <d.carpay@xs4all.nl>
2023-01-15 15:06:04 +0100

    Added plot function

--- FacebookGraph.py
+++ FacebookGraph.py
@@ -11,19 +11,21 @@ def inf_chance(r, i):
     return 1 - (1-i)**r
 
 
-class FacebookGraph:
+class SocialGraph:
+    """
+        Class to represent a social network,
+    """
     G = nx.read_edgelist('facebook_combined.txt', delimiter=' ')
-    i = 0.01
-    i_init = 0.1
-    time_steps = 10
-
+    i = 0.001
+    i_init = 0.0001
+    time_steps = 1000
 
     def __init__(self):
         nodes = self.G.nodes()
 
         # initialize node states
         states = np.zeros(len(nodes))
-        states[np.random.uniform(size = len(nodes)) < self.i_init] = 1
+        states[np.random.uniform(size=len(nodes)) < self.i_init] = 1
         node_states = dict(zip(nodes, states))
 
         nx.set_node_attributes(self.G, node_states, "state")
@@ -34,8 +36,11 @@ class FacebookGraph:
         self.infected_at_t = [self.inf_count]
         self.susceptible_at_t = [self.sus_count]
 
-
     def draw_graph(self, title, show=False):
+        """
+            Draw the graph, where each infected node is red, each uninfected
+            node is blue.
+        """
         node_colors = []
 
         for state in nx.get_node_attributes(self.G, "state").values():
@@ -44,30 +49,50 @@ class FacebookGraph:
             else:
                 node_colors.append('blue')
 
-        nx.draw(self.G, with_labels=False, node_color=node_colors, node_size=20)
+        nx.draw(self.G, with_labels=False, node_color=node_colors,
+                node_size=20)
         plt.savefig(title + ".png")
-        if show: plt.show()
+        if show:
+            plt.show()
 
+    def show_infected_plot(self):
+        """
+            Plot the amount of infected nodes in a graph.
+        """
+        plt.figure(figsize=(10, 10))
+        plt.plot(list(range(self.time_steps + 1)), self.infected_at_t)
+        plt.xlabel("Timestep t")
+        plt.ylabel("Amount infected nodes")
+        plt.title("Amount of infected nodes at timestep t")
+        plt.savefig("infectedplt.png")
+
+    def set_init_values(self, i, i_init):
+        self.i = i
+        self.i_init = i_init
 
     def make_timestep(self):
+        """
+            Make a single timestep. Infect new nodes and update statistics.
+        """
+
         inf_degree = []
         for n in self.G.nodes:
             if self.G.nodes[n]["state"] == 0:
                 neighbors = nx.all_neighbors(self.G, n)
                 neighbor_states = [self.G.nodes[neighbor]['state'] for
-                                neighbor in neighbors]
+                                   neighbor in neighbors]
                 n_inf_neighbors = neighbor_states.count(1)
                 total_neighbors = len(neighbor_states)
 
                 # newly infected
                 if np.random.uniform() < inf_chance(n_inf_neighbors,
-                                                         self.i):
+                                                    self.i):
                     self.node_states[n] = 1
                     inf_degree.append(total_neighbors)
 
         nx.set_node_attributes(self.G, self.node_states, "state")
         # track infected and susceptible counts
-        self.inf_count = np.sum(list(self.node_states.values()))
+        self.inf_count = int(np.sum(list(self.node_states.values())))
         self.sus_count = len(self.node_states) - self.inf_count
         self.infected_at_t.append(self.inf_count)
         self.susceptible_at_t.append(self.sus_count)
@@ -77,15 +102,16 @@ class FacebookGraph:
 
         return self.inf_count
 
+
 def main():
-    FbGraph = FacebookGraph()
-    # print(FbGraph.node_states)
-    FbGraph.draw_graph("Before", True)
+    FbGraph = SocialGraph()
+    # FbGraph.draw_graph("Before", True)
 
     for i in range(FbGraph.time_steps):
         print(f"Timestep {i}, infected nodes: {FbGraph.make_timestep()}")
 
-    FbGraph.draw_graph("After", True)
+    # FbGraph.draw_graph("After", True)
+    FbGraph.show_infected_plot()
 
 
 if __name__ == "__main__":
