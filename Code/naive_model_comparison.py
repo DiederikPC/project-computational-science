15fc2ab874701b1a16dc5f8a6c44cbe27f108e60
bramstibbe <bramstibbe@gmail.com>
2023-01-19 14:55:58 +0100

    Simulated spread 500 times and saved difference results (1 specific BA!!!)

--- Code/naive_model_comparison.py
+++ Code/naive_model_comparison.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from SocialGraph import SocialGraph
 
-sims, i, i_init, time_steps = 40, 0.01, 0.1, 50
+sims, i, i_init, time_steps = 500, 0.01, 0.1, 50
 
 # open the facebook and barabasi-albert networks
 facebook = SocialGraph("../Data/facebook_combined.txt", i, i_init, time_steps)
 BA = SocialGraph("../Data/barabasi_albert.txt", i, i_init, time_steps)
 
-surface = []
+total_diff = []
 for _ in range(sims):
     facebook.initialize_states()
     BA.initialize_states()
@@ -23,12 +23,21 @@ for _ in range(sims):
     BA_percent = [i / len(BA.G.nodes) for i in BA.infected_at_t]
 
     diffs = [np.abs(fb - ba) for fb, ba in zip(facebook_percent, BA_percent)]
-    surface.append(np.sum(diffs))
+    total_diff.append(np.sum(diffs))
 
+with open('../Data/total_difference_save.txt', 'w') as f:
+    for diff in total_diff:
+        f.write(str(diff) + "\n")
 
 plt.figure()
-plt.hist(surface, density=True)
+plt.title("Distribution of total differences of infection curves (FB vs BA)")
+plt.hist(total_diff, density=True, bins=50)
+plt.xlabel("Total difference (FB vs BA)")
+plt.ylabel("Frequency")
 plt.show()
+plt.savefig("../Plots/total_difference_distribution.png")
+
+print(total_diff)
 
 # # plotting spread on both networks
 # plt.figure(figsize=(10, 10))
