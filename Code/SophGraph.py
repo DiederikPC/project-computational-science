5c6583bb2b17e01fb94afd1b36445203fd396027
PhoebusProgrammer <d.carpay@xs4all.nl>
2023-01-23 13:15:11 +0100

    cleaned code up a bit

--- Code/SophGraph.py
+++ Code/SophGraph.py
@@ -3,24 +3,29 @@ import networkx as nx
 import numpy as np
 
 
-def soph_inf_chance(n_neigh, n_inf_neigh, global_decay, time_step, ave_degree):
-    """
-    Returns the infection chance given number of infected neighbors r and global decay rate
-    """
-    # probabilities read by eye from the paper we discussed
-    probs = [0, 0.014, 0.02, 0.021, 0.021, 0.02, 0.019, 0.018, 0.017, 0.016, 0.016, 0.016, 0.015, 0.015, 0.014, 0.014,
-             0.014, 0.013, 0.014]
-
-    # if the number of infected neighbors is larger than 18 we assume stable value of 0.014
-    if n_inf_neigh > 18:
-        return (0.014 * ave_degree) / ((1 + global_decay * time_step) * n_neigh)
-
-    return (probs[n_inf_neigh] * ave_degree) / ((1 + global_decay * time_step) * n_neigh)
 
 class SophGraph(SocialGraph):
+    def soph_inf_chance(self, n_neigh, n_inf_neigh,):
+        """
+        Returns the infection chance given number of infected neighbors r and
+        global decay rate
+        """
+        # probabilities read by eye from the paper we discussed
+        probs = [0, 0.014, 0.02, 0.021, 0.021, 0.02, 0.019, 0.018, 0.017,
+                 0.016, 0.016, 0.016, 0.015, 0.015, 0.014, 0.014, 0.014,
+                 0.013, 0.014]
+
+        # if the number of infected neighbors is larger than 18 we assume
+        # stable value of 0.014
+        if n_inf_neigh > 18:
+            return ((0.014 * self.ave_degree) /
+                   ((1 + self.decay_rate * self.t) * n_neigh))
+
+        return ((probs[n_inf_neigh] * self.ave_degree) /
+               ((1 + self.decay_rate * self.t) * n_neigh))
     def __init__(self, edgelist, i, i_init, time_steps, decay_rate):
         super().__init__(edgelist, i, i_init, time_steps)
-        self.t = 0
+        self.t = 1
         self.decay_rate = decay_rate
         self.ave_degree = self.G.number_of_edges() / self.G.number_of_nodes()
 
@@ -38,8 +43,8 @@ class SophGraph(SocialGraph):
                 total_neighbors = len(neighbor_states)
 
                 # newly infected
-                if np.random.uniform() < soph_inf_chance(total_neighbors, n_inf_neighbors, self.global_decay,
-                                                         self.current_timestep, self.ave_degree):
+                if np.random.uniform() < self.soph_inf_chance(total_neighbors,
+                                                              n_inf_neighbors):
                     self.node_states[n] = 1
                     inf_degree.append(total_neighbors)
 
