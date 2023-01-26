68366a64cc2ab15fb757bc5bd92290530eeedd55
Diederik Carpay <d.carpay@xs4all.nl>
2023-01-26 12:35:36 +0100

    fixed weird

--- Code/get_metrics.py
+++ Code/get_metrics.py
@@ -13,7 +13,7 @@ i, i_init, time_steps, decay_rate, sims = 0.01, 0.001, 30, 0.01, 5
 threshold = 300
 
 def get_metrics(is_SI, is_BA,i,i_init,time_steps,decay_rate,sims,threshold):
-    
+
     # THE FOUR METRICS
     perct_list = []
     early_deg = []
@@ -33,13 +33,13 @@ def get_metrics(is_SI, is_BA,i,i_init,time_steps,decay_rate,sims,threshold):
             graph = SophGraph(i, i_init, time_steps, decay_rate, "facebook_combined.txt", is_BA)
 
         for u in range(time_steps):
-            graph.make_timestep()   
-        
+            graph.make_timestep()
+
         speed_t = graph.infected_at_t[:np.where((np.absolute(np.array(graph.infected_at_t) - graph.infected_at_t[-1]) < threshold))[0][0]]
         difs = [speed_t[i+1] - speed_t[i] for i in range(len(speed_t)-2)]
         speed_list.append(np.mean(difs))
 
-        if not is_SI: 
+        if not is_SI:
             reach_list.append(graph.find_furthest_inf_node())
 
         early_deg.append(np.mean(graph.inf_degree_avg[:10]))
