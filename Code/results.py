a8e53d7ef3eb58a62f6a788436e9dd00c134d333
ness24000 <nestor.chulvi@gmail.com>
2023-01-27 15:36:24 +0100

    moved function

--- Code/results.py
+++ Code/results.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from get_metrics import get_metrics
 
+
 i, i_init, time_steps, decay_rate, sims = 0.01, 0.001, 30, 0.01, 2
 threshold = 30
-
 
 parameter_range = np.linspace(0, 0.1, 3)
 metric_names = ['infec_list', 'early_deg', 'speed_list']
@@ -27,14 +27,4 @@ plt.legend()
 plt.show()
 
 
-# function to get average error percentage
-def average_error_percent(FB, BA):
-    """
-    Returns the average percentage difference between Facebook and
-    Barabasi-Albert values. Arguments are a list for a metric for FB and a list
-    for the same metric for BA
-    """
-    return np.mean((np.abs(np.array(BA) - np.array(FB)) / np.array(BA)))
 
-
-average_error_percent(SI_FB_results['speed_list'], SI_BA_results['speed_list'])
