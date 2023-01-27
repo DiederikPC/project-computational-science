f5df830ddccc23085b834a0c38aca43fa5ac94f8
ness24000 <nestor.chulvi@gmail.com>
2023-01-27 17:26:16 +0100

    initial results

--- Code/results.py
+++ Code/results.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 from get_metrics import get_metrics
 import pandas as pd
 
-
-i, i_init, time_steps, decay_rate, sims = 0.01, 0.001, 30, 0.01, 2
+# PARAMETER i
+i, i_init, time_steps, decay_rate, sims = 0.01, 0.001, 30, 0.1, 15
 threshold = 30
-
-parameter_range = np.linspace(0, 0.5, 3)
+parameter_range = np.linspace(0, 0.5, 20)
 metric_names = ['infec_list', 'early_deg', 'speed_list']
 SI_FB_results = {'infec_list': [], 'early_deg': [], 'speed_list': []}
 SI_BA_results = {'infec_list': [], 'early_deg': [], 'speed_list': []}
@@ -22,17 +21,13 @@ for i in parameter_range:
         SI_FB_results[name].append(np.mean(SI_FB[name]))
         SI_BA_results[name].append(np.mean(SI_BA[name]))
 
-data = pd.DataFrame(SI_BA_results)
-data.insert(0, "i", parameter_range)
-data.to_csv('..GfG.csv',)
+data = pd.DataFrame({'i':parameter_range,'infec_list_FB':SI_FB_results['infec_list'],
+'infec_list_BA':SI_BA_results['infec_list'],'early_deg_FB':SI_FB_results['early_deg'],
+'early_deg_BA':SI_BA_results['early_deg'],'speed_list_FB':SI_FB_results['speed_list'],
+'speed_list_BA':SI_BA_results['speed_list']})
 
+data.to_csv('../Data/Results/results_SI_I.csv',index = False)
 
-
-# plt.figure()
-# plt.plot(parameter_range, SI_FB_results['speed_list'], label='FB')
-# plt.plot(parameter_range, SI_BA_results['speed_list'], label='BA')
-# plt.legend()
-# plt.show()
 
 
 
