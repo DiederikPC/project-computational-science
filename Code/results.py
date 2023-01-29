a4f92e9cd58340abed25f3869939c0d2ead65407
ness24000 <nestor.chulvi@gmail.com>
2023-01-29 17:12:46 +0100

    final code for results(?)

--- Code/results.py
+++ Code/results.py
@@ -1,7 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from get_metrics import get_metrics
+from get_metrics import get_param_results
 import pandas as pd
+
+parameters = ['i','i_init','i','i_init','decay']
+parameters_range = [np.arange(0.1,0.21,0.01),np.arange(0.1,0.21,0.01),np.arange(0.5,2.5,0.1),
+                np.arange(0.1,0.21,0.01),np.arange(0.1,0.21,0.01)]
+models = [True, True, False, False, False]
+
+for i in range(len(parameters)):
+    get_param_results(parameters[i],parameters_range[i],model = models[i])
 
 
 
