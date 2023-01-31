06c73009043e6437b3689cf43faa1b4bf02ad197
Rico <ricomiquelva@gmail.com>
2023-01-31 11:15:13 +0100

    added influential scores

--- Code/structure_analysis.py
+++ Code/structure_analysis.py
@@ -76,8 +76,8 @@ def get_degree_dist(FB, BA, BA_averaging=False):
     # create BA subplots, take average degrees from 30 BA's if averaging is True
     if not BA_averaging:
         BA_degree_dist = sorted([degree for _, degree in BA.degree()], reverse=True)
-        axs[1, 0].plot(BA_degree_dist, "tab:red", marker="o")
-        axs[1, 1].hist(BA_degree_dist, log=True, bins=50, color="tab:red")
+        axs[1, 0].plot(BA_degree_dist, "tab:orange", marker="o")
+        axs[1, 1].hist(BA_degree_dist, log=True, bins=50, color="tab:orange")
     else:
         BA_degree_dists = []
         for _ in range(30):
@@ -85,8 +85,8 @@ def get_degree_dist(FB, BA, BA_averaging=False):
             BA_degree_dists.append(sorted([degree for _, degree in BA.degree()], reverse=True))
         
         BA_avg_degree_dist = [np.mean([n[i] for n in BA_degree_dists]) for i in range(len(BA_degree_dists[0]))]
-        axs[1, 0].plot(BA_avg_degree_dist, "tab:red", marker="o")
-        axs[1, 1].hist(BA_avg_degree_dist, log=True, bins=50, color="tab:red")
+        axs[1, 0].plot(BA_avg_degree_dist, "tab:orange", marker="o")
+        axs[1, 1].hist(BA_avg_degree_dist, log=True, bins=50, color="tab:orange")
 
     # set titles and axis labels for all subplots
     axs[0, 0].set_title('FB degree-rank plot')
@@ -107,10 +107,10 @@ def get_degree_dist(FB, BA, BA_averaging=False):
 
     # FB and BA subplots combined
     axs[2, 0].plot(FB_degree_dist)
-    axs[2, 0].plot(BA_avg_degree_dist, "tab:red")
+    axs[2, 0].plot(BA_avg_degree_dist, "tab:orange")
 
     axs[2, 1].hist(FB_degree_dist, log=True, bins=50)
-    axs[2, 1].hist(BA_avg_degree_dist, log=True, bins=50, color='tab:red')
+    axs[2, 1].hist(BA_avg_degree_dist, log=True, bins=50, color='tab:orange')
 
     axs[2, 0].set_title('FB/BA degree-rank plot')
     axs[2, 0].set_ylabel("Degree")
@@ -130,52 +130,66 @@ def get_degree_dist(FB, BA, BA_averaging=False):
 def get_centralities(FB, BA, BA_averaging=False):
         
     ##### Degree Centrality #####
-    # FB_degree = nx.degree_centrality(FB)
-    # FB_degree = sorted(FB_degree.items(), key=lambda x: x[1], reverse=True)
+    FB_degree = nx.degree_centrality(FB)
+    FB_degree = sorted(FB_degree.items(), key=lambda x: x[1], reverse=True)
     # FB_degree = [score for _, score in FB_degree]
 
-    # BA_degree = nx.degree_centrality(BA)
-    # BA_degree = sorted(BA_degree.items(), key=lambda x: x[1], reverse=True)
+    BA_degree = nx.degree_centrality(BA)
+    BA_degree = sorted(BA_degree.items(), key=lambda x: x[1], reverse=True)
     # BA_degree = [score for _, score in BA_degree]
 
     # plt.hist(FB_degree, log=True, bins=50)
+    # plt.show()
+    # plt.plot(FB_degree)
+    # plt.show()
     # plt.hist(BA_degree, log=True, bins=50)
     # plt.show()
+    # plt.plot(BA_degree)
+    # plt.show()
+
+    # print(np.mean(FB_degree))
+    # print(np.std(FB_degree))
+    # print(np.mean(BA_degree))
+    # print(np.std(BA_degree))
 
     ##### Betweenness Centrality #####
     FB_betweenness = nx.betweenness_centrality(FB)
     FB_betweenness = sorted(FB_betweenness.items(), key=lambda x: x[1], reverse=True)
-    FB_betweenness = [score for _, score in FB_betweenness]
+    # FB_betweenness = [score for _, score in FB_betweenness]
 
     BA_betweenness = nx.betweenness_centrality(BA)
     BA_betweenness = sorted(BA_betweenness.items(), key=lambda x: x[1], reverse=True)
-    BA_betweenness = [score for _, score in BA_betweenness]
+    # BA_betweenness = [score for _, score in BA_betweenness]
 
     # plt.hist(FB_betweenness, log=True, bins=50)
-    plt.plot(FB_betweenness)
-    plt.show()
+    # plt.show()
+    # plt.plot(FB_betweenness)
+    # plt.yscale("log")
+    # plt.show()
     # plt.hist(BA_betweenness, log=True, bins=50)
-    plt.plot(BA_betweenness)
-    plt.show()
+    # plt.show()
+    # plt.plot(BA_betweenness)
+    # plt.yscale("log")
+    # plt.show()
 
-    print(np.mean(FB_betweenness))
-    print(np.std(FB_betweenness))
-    print(np.mean(BA_betweenness))
-    print(np.std(BA_betweenness))
+    # print(np.mean(FB_betweenness))
+    # print(np.std(FB_betweenness))
+    # print(np.mean(BA_betweenness))
+    # print(np.std(BA_betweenness))
 
     ##### Closeness Centrality #####
-    # FB_closeness = nx.closeness_centrality(FB)
-    # FB_closeness = sorted(FB_closeness.items(), key=lambda x: x[1], reverse=True)
+    FB_closeness = nx.closeness_centrality(FB)
+    FB_closeness = sorted(FB_closeness.items(), key=lambda x: x[1], reverse=True)
     # FB_closeness = [score for _, score in FB_closeness]
 
-    # BA_closeness = nx.closeness_centrality(BA)
-    # BA_closeness = sorted(BA_closeness.items(), key=lambda x: x[1], reverse=True)
+    BA_closeness = nx.closeness_centrality(BA)
+    BA_closeness = sorted(BA_closeness.items(), key=lambda x: x[1], reverse=True)
     # BA_closeness = [score for _, score in BA_closeness]
 
-    # # plt.hist(FB_closeness, bins=50)
+    # plt.hist(FB_closeness, bins=50)
     # plt.plot(FB_closeness)
     # plt.show()
-    # # plt.hist(BA_closeness, bins=50)
+    # plt.hist(BA_closeness, bins=50)
     # plt.plot(BA_closeness)
     # plt.show()
 
@@ -183,6 +197,44 @@ def get_centralities(FB, BA, BA_averaging=False):
     # print(np.std(FB_closeness))
     # print(np.mean(BA_closeness))
     # print(np.std(BA_closeness))
+
+
+    ####################   INFLUENTIAL SCORES #####################
+    # calculate individual node influence scores (the lower the better)
+
+    # FB centralities
+    FB_centralities = [FB_degree, FB_betweenness, FB_closeness]
+    FB_centrality_scores = {str(node): 0 for node in FB.nodes()}
+
+    for centrality in FB_centralities:
+        for i, (node, _) in enumerate(centrality):
+            FB_centrality_scores[str(node)] += i
+
+    FB_centrality_scores = sorted(FB_centrality_scores.items(),
+                                key=lambda x: x[1])
+    FB_centrality_scores = [score for (_, score) in FB_centrality_scores]
+
+    # BA centralities
+    BA_centralities = [BA_degree, BA_betweenness, BA_closeness]
+    BA_centrality_scores = {str(node): 0 for node in BA.nodes()}
+
+    for centrality in BA_centralities:
+        for i, (node, _) in enumerate(centrality):
+            BA_centrality_scores[str(node)] += i
+
+    BA_centrality_scores = sorted(BA_centrality_scores.items(),
+                                key=lambda x: x[1])
+    BA_centrality_scores = [score for (_, score) in BA_centrality_scores]
+
+
+    plt.plot(FB_centrality_scores)
+    plt.show()
+    plt.plot(BA_centrality_scores)
+    plt.show()
+    plt.hist(FB_centrality_scores, log=False, bins=50)
+    plt.show()
+    plt.hist(BA_centrality_scores, log=False, bins=50)
+    plt.show()
 
     return
 
