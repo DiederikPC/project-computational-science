7ddf757f1c768a5aac5fa9045ba3cf93afed6ec6
PhoebusProgrammer <d.carpay@xs4all.nl>
2023-02-01 10:42:10 +0100

    Added reach for params

--- Code/get_metrics.py
+++ Code/get_metrics.py
@@ -53,8 +53,8 @@ def get_metrics(is_SI, is_BA, i, i_init, time_steps, decay_rate, sims,
         speed_list.append(mean_speed)
 
 
-        # if not is_SI:
-        #     reach_list.append(graph.determine_reach())
+        if not is_SI:
+            reach_list.append(graph.determine_reach())
 
         early_deg.append(np.mean(graph.inf_degree_avg[:10]))
         infec_list.append(graph.inf_count)
@@ -79,11 +79,11 @@ def get_param_results(parameter, parameter_range, is_SI):
     sims = 20
     threshold = 30
     params = {"i": 0.01, "i_init": 0.001, "time_steps": 30, "decay_rate": 0.1}
-    if not is_SI: 
+    if not is_SI:
         params['i'] = 1
-    metric_names = ['infec_list', 'early_deg', 'speed_list']
-    FB_results = {'infec_list': [], 'early_deg': [], 'speed_list': []}
-    BA_results = {'infec_list': [], 'early_deg': [], 'speed_list': []}
+    metric_names = ['infec_list', 'early_deg', 'speed_list', 'reach_list']
+    FB_results = {'infec_list': [], 'early_deg': [], 'speed_list': [], 'reach_list': []}
+    BA_results = {'infec_list': [], 'early_deg': [], 'speed_list': [], 'reach_list': []}
 
     for j in parameter_range:
         params[parameter] = j
@@ -100,16 +100,19 @@ def get_param_results(parameter, parameter_range, is_SI):
 
     raw_index = [np.repeat(round(x,4),sims) for x in parameter_range]
     index = [elem for sublist in raw_index for elem in sublist]
-
+    print(FB_results)
     data = pd.DataFrame({f'{parameter}': index,
                         'infec_list_FB': FB_results['infec_list'],
                         'infec_list_BA': BA_results['infec_list'],
                         'early_deg_FB': FB_results['early_deg'],
                         'early_deg_BA': BA_results['early_deg'],
                         'speed_list_FB': FB_results['speed_list'],
-                        'speed_list_BA': BA_results['speed_list']})
-    
+                        'speed_list_BA': BA_results['speed_list'],
+                        'reach_list_FB': FB_results['reach_list'],
+                        'reach_list_BA': BA_results['reach_list']})
+
+
     model = 'SI' if is_SI else 'Soph'
-    data.to_csv(f'../Data/Results/results_{model}_{parameter}.csv', index=False)
+    data.to_csv(f'../Data/Results/results_{model}_{parameter}_reach.csv', index=False)
 
 
--- Code/running_model.py
+++ Code/running_model.py
@@ -5,10 +5,10 @@ from get_metrics import get_param_results
 import pandas as pd
 
 steps = 20
-parameters = ['i','i_init','i','i_init','decay_rate']
-parameters_range = [np.linspace(0,0.042,steps),np.linspace(0,0.03,steps),np.linspace(0.5,2.5,steps),
+parameters = ['i','i_init','decay_rate']
+parameters_range = [np.linspace(0.5,2.5,steps),
                 np.linspace(0.01,0.21,steps),np.linspace(0.01,0.21,steps)]
-models = [True, True, False, False, False]
+models = [False, False, False]
 
 for i in range(len(parameters)):
     get_param_results(parameters[i],parameters_range[i],models[i])
